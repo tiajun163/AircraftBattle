285f3e56e373d6af20054a85d8af1f2a5004a9aa
tiajun163 <tiajun163@163.com>
2020-04-01 16:37:51 +0800

    Initial commit

--- adding_ship_image/alien_invasion.py
+++ adding_ship_image/alien_invasion.py
@@ -1,22 +1,43 @@
 import  sys#导入sys包
 import  pygame#导入pygame包
-
+from adding_ship_image.settings import Settings
+#
+# def run_game():
+#     #初始化游戏并创建一个屏幕对象
+#     pygame.init()#初始化背景设置
+#     #调用pygame.display.set_mode()方法创建一个screen窗口
+#     screen=pygame.display.set_mode((800,800))
+#     pygame.display.set_caption('Alien Invasion')
+#     bg_color=(230,238,230)#设置屏幕 颜色
+#     #开始游戏的主循环
+#     while True:
+#          #监视键盘和鼠标事件
+#         for event in pygame.event.get():#使用pygame.event.get()方法，检查pygame访问事件
+#             #检测到退出 使用pgame.QUIT()和sys.exit()方法退出游戏
+#             if event.type==pygame.QUIT:
+#                 sys.exit()#退出游戏
+#             screen.fill(bg_color)#使用screnn.fill()方法填充背景颜色
+#             #让最近绘制的屏幕可见
+#             pygame.display.flip()
+# #初始化游戏并开始游戏
+# run_game()
+#----------------------------------------------------------------------------------------------------------------
 def run_game():
-    #初始化游戏并创建一个屏幕对象
-    pygame.init()#初始化背景设置
-    #调用pygame.display.set_mode()方法创建一个screen窗口
-    screen=pygame.display.set_mode((800,800))
-    pygame.display.set_caption('Alien Invasion')
-    bg_color=(230,238,230)#设置屏幕 颜色
-    #开始游戏的主循环
+    #初始化pygame,设置和屏幕对象
+    pygame.init()
+    ai_settings=Settings()
+    screen=pygame.display.set_mode((
+        ai_settings.screen_width,
+        ai_settings.screen_height))
+    pygame.display.set_caption('飞机大战')
+    #开始游戏循环
     while True:
-         #监视键盘和鼠标事件
+        # 监视键盘和鼠标事件
         for event in pygame.event.get():#使用pygame.event.get()方法，检查pygame访问事件
             #检测到退出 使用pgame.QUIT()和sys.exit()方法退出游戏
             if event.type==pygame.QUIT:
                 sys.exit()#退出游戏
-            screen.fill(bg_color)#使用screnn.fill()方法填充背景颜色
+            screen.fill(ai_settings.bg_color)#使用screnn.fill()方法填充背景颜色
             #让最近绘制的屏幕可见
             pygame.display.flip()
-#初始化游戏并开始游戏
 run_game()
---
+++ adding_ship_image/settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    ''''创建存储飞机大战的所有设置类'''
+    def __init__(self):
+        #初始化游戏设置
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color=(230,230,230)
