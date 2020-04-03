983da88841eea4af1c6d28b4f98e59e8f0462163
tiajun163 <tiajun163@163.com>
2020-04-03 10:47:25 +0800

    Initial commit

--- game_functions/alien_invasion.py
+++ game_functions/alien_invasion.py
@@ -1,2 +1,3 @@
 import  pygame
 from game_functions import game_function as gf
+from  game_functions.settings import Settings
---
+++ game_functions/ship.py
@@ -0,0 +1,5 @@
+import  pygame
+class Ship():
+    def __init__(self,screen):
+        self.screen=screen
+        self.image=pygame.image.load('images/ship')
